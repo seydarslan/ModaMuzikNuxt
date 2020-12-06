37488f6dd713ed2c783dd444528d75b385e50b09
seydarslan <seyda.arslann98@gmail.com>
2020-12-06 15:17:05 +0300

    modamuzik with nuxt version

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+<SideMenu></SideMenu>
+    
+  </div>
+</template>
+
+<script>
+  import SideMenu from '~/components/SideMenu'
+  export default {
+    components: { SideMenu  }
+  }
+</script>
+
+<style>
+
+</style>
